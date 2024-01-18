9fdd46d2fcdfb47283527219f16c1f936e089534
Dani Gleba <daniglebapuig@gmail.com>
2024-01-18 11:49:15 +0100

    fix: typo in subheader

--- components/Hero.js
+++ components/Hero.js
@@ -12,7 +12,7 @@ export default function Hero() {
             <div>
                 <div className="flex flex-col justify-center items-center lg:items-start space-y-8">
                     <h1 className={livvic.className}>Filter user feedback from your comment section</h1>
-                    <p className="text-[#acacac] text-center lg:text-left md:text-lg">Your YouTube videos" comment section is filled with great user insight. But filtering the thousands of meaningless comments is too time-consuming. Let our AI do that for you and get all that user feedback, questions and bug reports in your inbox.</p>
+                    <p className="text-[#acacac] text-center lg:text-left md:text-lg">Your YouTube videos' comment section is filled with great user insight. But filtering the thousands of meaningless comments is too time-consuming. Let our AI do that for you and get all that user feedback, questions and bug reports in your inbox.</p>
                     <button onClick={() => router.push("/filter-comments")}>Get started</button>
                 </div>
                 <Testimonials />
