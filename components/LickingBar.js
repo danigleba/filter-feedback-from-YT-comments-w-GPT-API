cff4dca00f20150ce5a38bac7b3b6e2d702185bd
Dani Gleba <daniglebapuig@gmail.com>
2024-02-04 17:46:50 +0100

    add: vercel pro

--- components/LickingBar.js
+++ components/LickingBar.js
@@ -1,6 +1,6 @@
 export default function LickingBar({positivePercentage, negativePercentage}) {
     return (
-        <div className="pb-12">
+        <div className="pb-12" >
             <p className="section-title pb-2">Positive Vs. Negative comments</p>
             <div className="flex items-center bg-white border border-white rounded-full overflow-hidden font-bold text-sm text-white h-8">
                 <div className="graph-line bg-gradient-to-r from-rose-400 to-orange-300" style={{ width: `${positivePercentage}%` }}>
